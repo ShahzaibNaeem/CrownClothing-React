708243f459cc935d756c18752987be21d9c88a3e
Shahzaib <chorli635@gmail.com>
2022-08-27 16:55:45 +0500

    Changed useState to useReducer in user & cart

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,14 +1,40 @@
-import { createContext,useState,useEffect } from "react";
+import { createContext,useReducer,useEffect } from "react";
 import { onAuthStateChangedlistener,createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducers/reducer.util";
 
 export const UserContext=createContext({
     currentUser:null,
     setCurrentUser:()=>null
 });
 
+export const USER_ACTION_TYPES={
+  SET_CURRENT_USER:"SET_CURRENT_USER"
+}
+
+const userReducer=(state,action)=>{
+  const {type,payload}=action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return{
+        ...state,currentUser:payload
+      }
+    default:
+      throw new Error(`Unhandled type in ${type} in useReducer`)
+  }
+}
+
+const INITIAL_STATE={
+  currentUser:null
+}
 
 export const UserProvider=({children})=>{
-    const [currentUser, setCurrentUser] = useState(null);
+  const [{currentUser}, dispatch] = useReducer(userReducer,INITIAL_STATE)
+
+  const setCurrentUser=(user)=>{
+  dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER,user))
+  }
+   
     const value={currentUser,setCurrentUser};
 
     useEffect(()=>{
@@ -18,6 +44,7 @@ export const UserProvider=({children})=>{
                 }
             setCurrentUser(user);
           });  
+      
           return unsubscribe;
           // eslint-disable-next-line
         },[])
