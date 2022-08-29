d3aebd6c6731b387bf8092bd2cf4dd3aeaf94886
Shahzaib <chorli635@gmail.com>
2022-08-29 17:12:09 +0500

    Added custom loggerMiddleware  in redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
 import {legacy_createStore as createStore, applyMiddleware, compose } from 'redux'
-import logger from "redux-logger"
+// import logger from "redux-logger"
 import { rootReducer } from "./root-reducer"
 
+//MiddleWare are chained curryfunctions in Redux
+const loggerMiddleware=(store)=>(next)=>(action)=>{
+    if(!action.type) next(action)
+ 
+     console.log("type: " , action.type)
+     console.log("payload: " , action.payload)
+     console.log("currentState: " , store.getState())
+ 
+     next(action)
+     console.log("nextState:", store.getState())
+ 
+ } 
 
-const middleWares=[logger]
+const middleWares=[loggerMiddleware]
 const composedEnhancers=compose(applyMiddleware(...middleWares))
 
 export const store=createStore(rootReducer,undefined,composedEnhancers)
